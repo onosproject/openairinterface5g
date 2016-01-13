82b4dba261062442c93a08ebc7ca5d0b080f5d67
Cedric Roux <roux@eurecom.fr>
2016-01-13 15:37:48 +0100

    add extern "C" in ff-mac-callback.h

--- openair2/LAYER2/MAC/ff-mac-callback.h
+++ openair2/LAYER2/MAC/ff-mac-callback.h
@@ -1,6 +1,10 @@
 #ifndef FF_MAC_CALLBACK_H
 #define FF_MAC_CALLBACK_H
 
+#if defined (__cplusplus)
+extern "C" {
+#endif
+
 /* these are the callback function types from the scheduler to openair */
 
 /* sched */
@@ -15,5 +19,9 @@ typedef void (CschedLcReleaseCnf_callback_t)(void *callback_data, const struct C
 typedef void (CschedUeReleaseCnf_callback_t)(void *callback_data, const struct CschedUeReleaseCnfParameters *params);
 typedef void (CschedUeConfigUpdateInd_callback_t)(void *callback_data, const struct CschedUeConfigUpdateIndParameters *params);
 typedef void (CschedCellConfigUpdateInd_callback_t)(void *callback_data, const struct CschedCellConfigUpdateIndParameters *params);
+
+#if defined (__cplusplus)
+}
+#endif
 
 #endif /* FF_MAC_CALLBACK_H */
