e7974775baead24f6de04b01829d9413c776ec4d
Matthieu Kanj <Matthieu.kanj@b-com.com>
2017-06-01 10:19:53 +0200

    modification of impl_defs_lte_nb_iot.h / adding NPBCH structur

--- openair1/PHY/impl_defs_lte_nb_iot.h
+++ openair1/PHY/impl_defs_lte_nb_iot.h
@@ -220,3 +220,14 @@ typedef struct {
 
 } NB_DL_FRAME_PARMS;
 
+#define NPBCH_A 34 
+
+typedef struct {
+  uint8_t npbch_d[66+(3*(16+NPBCH_A))];
+  uint8_t npbch_w[3*3*(16+NPBCH_A)];
+  uint8_t npbch_e[1600];
+} NB_IoT_eNB_NPBCH;
+
+
+#endif
+
