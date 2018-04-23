010f7fc2d4f1bd6d60a7bd36a4d3545f304ad84c
Robert Schmidt <robert.schmidt@eurecom.fr>
2018-04-23 16:59:22 +0200

    FlexRAN: Handle slice configuration
    Incoming slice configuration changes are buffered in an intermediate structure.
    The MAC scheduler periodically polls the agent about changes. If there has been
    a slice configuration change, apply it.

--- openair2/LAYER2/MAC/eNB_scheduler.c
+++ openair2/LAYER2/MAC/eNB_scheduler.c
@@ -676,6 +676,10 @@ eNB_dlsch_ulsch_scheduler(module_id_t module_idP, frame_t frameP,
   for (CC_id = 0; CC_id < MAX_NUM_CCs; CC_id++)
       allocate_CCEs(module_idP, CC_id, subframeP, 0);
 
+  if (mac_agent_registered[module_idP] && subframeP == 9) {
+    flexran_agent_slice_update(module_idP);
+  }
+
   stop_meas(&RC.mac[module_idP]->eNB_scheduler);
 
   VCD_SIGNAL_DUMPER_DUMP_FUNCTION_BY_NAME
