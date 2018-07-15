0e6d35810bc8dadf6fb6174da16ec948dd4ca707
Calvin HSU <Kai-Hsiang.Hsu@eurecom.fr>
2018-07-16 00:22:27 +0200

    Add OAI license and author(s) name, contact information

--- openair2/LAYER2/MAC/extern_NB_IoT.h
+++ openair2/LAYER2/MAC/extern_NB_IoT.h
@@ -1,9 +1,28 @@
-
+/*
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the OAI Public License, Version 1.0  (the "License"); you may not use this file
+ * except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.openairinterface.org/?page_id=698
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *-------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
 /*! \file extern_NB_IoT.h
  * \brief MAC extern
- * \author  NTUST BMW Lab./
- * \date 2017
- * \email: 
+ * \author  NTUST BMW Lab./Nick HO, Xavier LIU, Calvin HSU
+ * \date 2017 - 2018
+ * \email: nick133371@gmail.com, sephiroth7277@gmail.com , kai-hsiang.hsu@eurecom.fr
  * \version 1.0
  *
  */
