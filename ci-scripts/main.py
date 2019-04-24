400dfb57c637e3a32cf785b7fad9794ca5b90edf
Boris Djalal <boris.djalal@eurecom.fr>
2019-04-24 14:55:46 +0200

    Adding the mode in xml files.
    Signed-off-by: Boris Djalal <boris.djalal@eurecom.fr>

--- ci-scripts/main.py
+++ ci-scripts/main.py
@@ -3021,6 +3021,9 @@ def Usage():
 	print('  --XMLTestFile=[XML Test File to be run]')
 	print('------------------------------------------------------------')
 
+def GetModeFromXML():
+	SSH.mode = test.findtext('mode')
+
 def CheckClassValidity(action,id):
 	if action != 'Build_eNB' and action != 'Initialize_eNB' and action != 'Terminate_eNB' and action != 'Initialize_UE' and action != 'Terminate_UE' and action != 'Attach_UE' and action != 'Detach_UE' and action != 'Build_OAI_UE' and action != 'Initialize_OAI_UE' and action != 'Terminate_OAI_UE' and action != 'Initialize_OAI_eNB' and action != 'Ping' and action != 'Iperf' and action != 'Reboot_UE' and action != 'Initialize_HSS' and action != 'Terminate_HSS' and action != 'Initialize_MME' and action != 'Terminate_MME' and action != 'Initialize_SPGW' and action != 'Terminate_SPGW'  and action != 'Initialize_CatM_module' and action != 'Terminate_CatM_module' and action != 'Attach_CatM_module' and action != 'Detach_CatM_module' and action != 'Ping_CatM_module' and action != 'IdleSleep':
 		logging.debug('ERROR: test-case ' + id + ' has wrong class ' + action)
@@ -3139,7 +3142,13 @@ while len(argvs) > 1:
 		Usage()
 		sys.exit(0)
 	elif re.match('^\-\-mode=(.+)$', myArgv, re.IGNORECASE):
+		mode = GetModeFromXML()
+		print('mode: ' + mode)
 		matchReg = re.match('^\-\-mode=(.+)$', myArgv, re.IGNORECASE)
+		if matchReg and mode is not None:
+			print('Warning: the mode is defined in both xml file and command line')
+			print('ignoring the mode defined in the xml file')
+		print('mode: ' + mode)
 		mode = matchReg.group(1)
 	elif re.match('^\-\-eNBIPAddress=(.+)$', myArgv, re.IGNORECASE):
 		matchReg = re.match('^\-\-eNBIPAddress=(.+)$', myArgv, re.IGNORECASE)
