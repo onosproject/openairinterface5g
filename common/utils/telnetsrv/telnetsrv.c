1fbc356fed2f8a50c09b22adb4459ba174cab15b
masayuki.harada <masayuki.harada@jp.fujitsu.com>
2019-11-13 15:24:37 +0900

    Remove command length limitation in telnetsrv.c

--- common/utils/telnetsrv/telnetsrv.c
+++ common/utils/telnetsrv/telnetsrv.c
@@ -510,7 +510,7 @@ int process_command(char *buf) {
   memset(cmdb,0,sizeof(cmdb));
   bufbck=strdup(buf);
   rt=CMDSTATUS_NOTFOUND;
-  j = sscanf(buf,"%9s %9s %9[^\t\n]",modulename,cmd,cmdb);
+  j = sscanf(buf,"%9s %9s %64[^\t\n]",modulename,cmd,cmdb);
 
   if (telnetparams.telnetdbg > 0)
     printf("process_command: %i words, module=%s cmd=%s, parameters= %s\n",j,modulename,cmd,cmdb);
