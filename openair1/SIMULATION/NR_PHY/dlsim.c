42fe75744dbe84500daf0ee4379422c5358cd6f8
Sakthivel Velumani <velumani@eurecom.fr>
2019-12-18 15:27:09 +0100

    dlsim update for initial sync

--- openair1/SIMULATION/NR_PHY/dlsim.c
+++ openair1/SIMULATION/NR_PHY/dlsim.c
@@ -364,6 +364,7 @@ int main(int argc, char **argv)
     case 'I':
       run_initial_sync=1;
       target_error_rate=0.1;
+      slot = 0;
       break;
 
     case 'L':
@@ -603,9 +604,10 @@ int main(int argc, char **argv)
     Sched_INFO.TX_req    = &gNB_mac->TX_req[0];
     nr_schedule_response(&Sched_INFO);
 
-    phy_procedures_gNB_TX(gNB,frame,slot,0);
-    
-    //nr_common_signal_procedures (gNB,frame,subframe);
+    if(!run_initial_sync)
+      phy_procedures_gNB_TX(gNB,frame,slot,0);
+    else
+      nr_common_signal_procedures (gNB,frame,slot);
     int txdataF_offset = (slot%2) * frame_parms->samples_per_slot_wCP;
 
     LOG_M("txsigF0.m","txsF0", gNB->common_vars.txdataF[0],frame_length_complex_samples_no_prefix,1,1);
