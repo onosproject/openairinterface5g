5104558437a7494d5d5d0cb8aefd875a5e19191c
Sakthivel Velumani <velumani@eurecom.fr>
2020-01-08 14:38:35 +0100

    timing advance set for 66 PRB

--- openair1/SCHED_NR/phy_procedures_nr_gNB.c
+++ openair1/SCHED_NR/phy_procedures_nr_gNB.c
@@ -313,6 +313,7 @@ void nr_fill_rx_indication(PHY_VARS_gNB *gNB, int frame, int slot_rx, int UE_id,
     case 106: timing_advance_update /= 16; break;
     case 217: timing_advance_update /= 32; break;
     case 273: timing_advance_update /= 32; break;
+    case 66: timing_advance_update /= 12; break;
     default: abort();
   }
 
