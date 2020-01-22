2082828a65f65e7a6f80ea044a6829759562e19f
Haruki NAOI <naoi.haruki@jp.fujitsu.com>
2020-01-22 18:07:19 +0900

    fix compilation.

--- nfapi/open-nFAPI/vnf/src/vnf_interface.c
+++ nfapi/open-nFAPI/vnf/src/vnf_interface.c
@@ -215,7 +215,7 @@ void init_server_eventfd(int *watch_fd_list, int client_num, char *path)
 	if ((conn_socket = socket(AF_UNIX, SOCK_STREAM, 0)) < 0)
 	{
 		perror("socket (init_server_eventfd)\n");
-                exit_fun("init_server_eventfd fatal: socket err");
+	  exit(1);
 	}
 
 	addr.sun_family = AF_UNIX;
@@ -226,7 +226,7 @@ void init_server_eventfd(int *watch_fd_list, int client_num, char *path)
 	if (bind(conn_socket, (const struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1)
 	{
 		perror("bind (init_server_eventfd)\n");
-                exit_fun("init_server_eventfd fatal: bind err");
+	  exit(1);
 	}
 	printf("bind completed (init_server_eventfd) \n");
 
@@ -235,7 +235,7 @@ void init_server_eventfd(int *watch_fd_list, int client_num, char *path)
 	if (listen(conn_socket, 16) == -1)
 	{
 		perror("listen (init_server_eventfd)");
-                exit_fun("init_server_eventfd fatal: listen err");
+	  exit(1);
 	}
 
 	printf("init_server_eventfd : now listening\n");
@@ -245,7 +245,7 @@ void init_server_eventfd(int *watch_fd_list, int client_num, char *path)
 		if ((fd = accept(conn_socket, NULL, NULL)) == -1)
 		{
 			perror("accept (init_server_eventfd)\n");
-                        exit_fun("init_server_eventfd fatal: accept err");
+		  exit(1);
 		}
 		printf("accept completed (init_server_eventfd)\n");
 
