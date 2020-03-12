6cc13fafb085c3616012273665f133fa85e8fb9b
Robert Schmidt <robert.schmidt@eurecom.fr>
2020-03-12 13:16:28 +0100

    Move configure_fapi_dl_pdu() and configure_fapi_dl_Tx() to gNB primitives

--- openair2/LAYER2/NR_MAC_gNB/mac_proto.h
+++ openair2/LAYER2/NR_MAC_gNB/mac_proto.h
@@ -80,18 +80,18 @@ void nr_schedule_css_dlsch_phytest(module_id_t   module_idP,
                                    frame_t       frameP,
                                    sub_frame_t   subframeP);
 
-int configure_fapi_dl_pdu(int Mod_id,
-                         int *CCEIndeces,
-                         nfapi_nr_dl_tti_request_body_t *dl_req,
-                         uint8_t *mcsIndex,
-                         uint16_t *rbSize,
-                         uint16_t *rbStart);
+int configure_fapi_dl_pdu(int Mod_idP,
+                          int *CCEIndex,
+                          nfapi_nr_dl_tti_request_body_t *dl_req,
+                          uint8_t *mcsIndex,
+                          uint16_t *rbSize,
+                          uint16_t *rbStart);
 
 void config_uldci(NR_BWP_Uplink_t *ubwp,nfapi_nr_pusch_pdu_t *pusch_pdu,nfapi_nr_dl_tti_pdcch_pdu_rel15_t *pdcch_pdu_rel15, dci_pdu_rel15_t *dci_pdu_rel15, int *dci_formats, int *rnti_types);
 
 void configure_fapi_dl_Tx(module_id_t Mod_idP,
-                          frame_t       frameP,
-                          sub_frame_t   slotP,
+                          frame_t frameP,
+                          sub_frame_t slotP,
                           nfapi_nr_dl_tti_request_body_t *dl_req,
                           nfapi_nr_pdu_t *tx_req,
                           int tbs_bytes,
