2cfb5aa989a9cc0954f3a7dcdf328e8ce031ed4a
Wu Jing <wu.jing@cn.fujitsu.com>
2020-04-03 14:35:40 +0900

    Fix segfault when free memory in vnf_p7

--- nfapi/open-nFAPI/vnf/src/vnf_p7.c
+++ nfapi/open-nFAPI/vnf/src/vnf_p7.c
@@ -290,9 +290,9 @@ void vnf_p7_rx_reassembly_queue_remove_old_msgs(vnf_p7_t* vnf_p7, vnf_p7_rx_reas
 			
 			NFAPI_TRACE(NFAPI_TRACE_INFO, "Deleting stale reassembly message (%u %u %d)\n", iterator->rx_hr_time, rx_hr_time, delta);
 
-			iterator = iterator->next;
 #ifndef PHY_RM
 			vnf_p7_rx_message_t* to_delete = iterator;
+			iterator = iterator->next;
 			// delete the message
 			uint16_t i;
 			for(i = 0; i < 128; ++i)
@@ -302,6 +302,8 @@ void vnf_p7_rx_reassembly_queue_remove_old_msgs(vnf_p7_t* vnf_p7, vnf_p7_rx_reas
 			}
 			vnf_p7_free(vnf_p7, to_delete);
 			to_delete = 0;
+#else
+                       iterator = iterator->next;
 #endif
 		}
 		else
