3c522a8ec8486c5cb5a08e77b4c0c231c3abd1ff
bussotti <jean-flavien.bussotti@eurecom.fr>
2020-06-04 19:05:56 +0200

    Update pdcp_benchmark.c

--- openair2/LAYER2/PDCP_v10.1.0/BENCHMARK/pdcp_benchmark.c
+++ openair2/LAYER2/PDCP_v10.1.0/BENCHMARK/pdcp_benchmark.c
@@ -26,84 +26,18 @@
  *  * \date 2020
  *  */
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 
+#include "openair2/UTIL/MEM/mem_block.h"
+#include "openair2/LAYER2/PDCP_v10.1.0/pdcp.h"
 
 
 
 #define DUMMY_BUFFER ((unsigned char*)"123456789")
 #define DUMMY_BUFFER_SIZE 10
 
-typedef struct {
-  struct mem_block_t *head;
-  struct mem_block_t *tail;
-  int                nb_elements;
-  char               name[LIST_NAME_MAX_CHAR];
-} list_t;
-
-typedef struct pdcp_s {
-  //boolean_t     instanciated_instance;
-  uint16_t       header_compression_profile;
-
-  /* SR: added this flag to distinguish UE/eNB instance as pdcp_run for virtual
-   * mode can receive data on NETLINK for eNB while eNB_flag = 0 and for UE when eNB_flag = 1
-   */
-  boolean_t is_ue;
-  boolean_t is_srb;
-
-  /* Configured security algorithms */
-  uint8_t cipheringAlgorithm;
-  uint8_t integrityProtAlgorithm;
-
-  /* User-Plane encryption key
-   * Control-Plane RRC encryption key
-   * Control-Plane RRC integrity key
-   * These keys are configured by RRC layer
-   */
-  uint8_t *kUPenc;
-  uint8_t *kRRCint;
-  uint8_t *kRRCenc;
-
-  uint8_t security_activated;
-
-  rlc_mode_t rlc_mode;
-  uint8_t status_report;
-  uint8_t seq_num_size;
-
-  logical_chan_id_t lcid;
-  rb_id_t           rb_id;
-  /*
-   * Sequence number state variables
-   *
-   * TX and RX window
-   */
-  pdcp_sn_t next_pdcp_tx_sn;
-  pdcp_sn_t next_pdcp_rx_sn;
-  pdcp_sn_t maximum_pdcp_rx_sn;
-  /*
-   * TX and RX Hyper Frame Numbers
-   */
-  pdcp_hfn_t tx_hfn;
-  pdcp_hfn_t rx_hfn;
-
-  /*
-   * SN of the last PDCP SDU delivered to upper layers
-   */
-  pdcp_sn_t  last_submitted_pdcp_rx_sn;
-
-  /*
-   * Following array is used as a bitmap holding missing sequence
-   * numbers to generate a PDCP Control PDU for PDCP status
-   * report (see 6.2.6)
-   */
-  uint8_t missing_pdu_bitmap[512];
-  /*
-   * This is intentionally signed since we need a 'NULL' value
-   * which is not also a valid sequence number
-   */
-  short int first_missing_pdu;
-
-} pdcp_t;
 
 
 pdcp_t pdcp_el;
