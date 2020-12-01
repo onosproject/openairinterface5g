d978175b22c2c51b1f8849d44f008e921c89817e
Shad Ansari <shad69@gmail.com>
2020-12-01 03:42:41 +0000

    SubscriptionRequest's ActionToBeSetup-List IE

--- openair2/RIC_AGENT/e2ap_handler.c
+++ openair2/RIC_AGENT/e2ap_handler.c
@@ -42,63 +42,62 @@
 int e2ap_handle_e2_setup_response(ric_agent_info_t *ric,uint32_t stream,
 				  E2AP_E2AP_PDU_t *pdu)
 {
-  E2AP_E2setupResponse_t *resp;
-  E2AP_E2setupResponseIEs_t *rie,**ptr;
+    E2AP_E2setupResponse_t *resp;
+    E2AP_E2setupResponseIEs_t *rie,**ptr;
 
-  DevAssert(pdu != NULL);
-  resp = &pdu->choice.successfulOutcome.value.choice.E2setupResponse;
+    DevAssert(pdu != NULL);
+    resp = &pdu->choice.successfulOutcome.value.choice.E2setupResponse;
 
-  E2AP_INFO("Received E2SetupResponse (ranid %u)\n",ric->ranid);
+    E2AP_INFO("Received E2SetupResponse (ranid %u)\n",ric->ranid);
 
-  for (ptr = resp->protocolIEs.list.array;
-       ptr < &resp->protocolIEs.list.array[resp->protocolIEs.list.count];
-       ptr++) {
-    rie = (E2AP_E2setupResponseIEs_t *)*ptr;
-    if (rie->id == E2AP_ProtocolIE_ID_id_GlobalRIC_ID) {
-      PLMNID_TO_MCC_MNC(&rie->value.choice.GlobalRIC_ID.pLMN_Identity,
-			ric->ric_mcc,ric->ric_mnc,ric->ric_mnc_digit_len);
-      //BIT_STRING_TO_INT32(&rie->value.choice.GlobalRIC_ID.ric_ID,ric->ric_id);
+    for (ptr = resp->protocolIEs.list.array;
+            ptr < &resp->protocolIEs.list.array[resp->protocolIEs.list.count];
+            ptr++) {
+        rie = (E2AP_E2setupResponseIEs_t *)*ptr;
+        if (rie->id == E2AP_ProtocolIE_ID_id_GlobalRIC_ID) {
+            PLMNID_TO_MCC_MNC(&rie->value.choice.GlobalRIC_ID.pLMN_Identity,
+                    ric->ric_mcc,ric->ric_mnc,ric->ric_mnc_digit_len);
+            //BIT_STRING_TO_INT32(&rie->value.choice.GlobalRIC_ID.ric_ID,ric->ric_id);
+        }
+        /* XXX: handle RANfunction IEs once we have some E2SM support. */
     }
-    /* XXX: handle RANfunction IEs once we have some E2SM support. */
-  }
 
-  E2AP_INFO("E2SetupResponse (ranid %u) from RIC (mcc=%u,mnc=%u,id=%u)\n",
-	    ric->ranid,ric->ric_mcc,ric->ric_mnc,ric->ric_id);
+    E2AP_INFO("E2SetupResponse (ranid %u) from RIC (mcc=%u,mnc=%u,id=%u)\n",
+        ric->ranid,ric->ric_mcc,ric->ric_mnc,ric->ric_id);
 
-  ric->state = RIC_ESTABLISHED;
+    ric->state = RIC_ESTABLISHED;
 
-  return 0;
+    return 0;
 }
 
 int e2ap_handle_e2_setup_failure(ric_agent_info_t *ric,uint32_t stream,
 				 E2AP_E2AP_PDU_t *pdu)
 {
-  E2AP_E2setupFailure_t *resp;
-  E2AP_E2setupFailureIEs_t *rie,**ptr;
-  long cause,cause_detail;
+    E2AP_E2setupFailure_t *resp;
+    E2AP_E2setupFailureIEs_t *rie,**ptr;
+    long cause,cause_detail;
 
-  DevAssert(pdu != NULL);
-  resp = &pdu->choice.unsuccessfulOutcome.value.choice.E2setupFailure;
+    DevAssert(pdu != NULL);
+    resp = &pdu->choice.unsuccessfulOutcome.value.choice.E2setupFailure;
 
-  E2AP_INFO("Received E2SetupFailure (ranid %u)\n",ric->ranid);
+    E2AP_INFO("Received E2SetupFailure (ranid %u)\n",ric->ranid);
 
-  for (ptr = resp->protocolIEs.list.array;
-       ptr < &resp->protocolIEs.list.array[resp->protocolIEs.list.count];
-       ptr++) {
-    rie = (E2AP_E2setupFailureIEs_t *)*ptr;
-    if (rie->id == E2AP_ProtocolIE_ID_id_Cause) {
-	cause = rie->value.choice.Cause.present;
-	cause_detail = rie->value.choice.Cause.choice.misc;
-    }
+    for (ptr = resp->protocolIEs.list.array;
+            ptr < &resp->protocolIEs.list.array[resp->protocolIEs.list.count];
+            ptr++) {
+        rie = (E2AP_E2setupFailureIEs_t *)*ptr;
+        if (rie->id == E2AP_ProtocolIE_ID_id_Cause) {
+            cause = rie->value.choice.Cause.present;
+            cause_detail = rie->value.choice.Cause.choice.misc;
+        }
     /* XXX: handle RANfunction IEs once we have some E2SM support. */
-  }
+    }
 
-  E2AP_INFO("E2SetupFailure (ranid %u) from RIC (cause=%ld,detail=%ld)\n",
-	    ric->ranid,cause,cause_detail);
+    E2AP_INFO("E2SetupFailure (ranid %u) from RIC (cause=%ld,detail=%ld)\n", ric->ranid,cause,cause_detail);
 
-  ric->state = RIC_FAILURE;
+    ric->state = RIC_FAILURE;
 
-  return 0;
+    return 0;
 }
 
 int e2ap_handle_ric_subscription_request(ric_agent_info_t *ric,uint32_t stream,
@@ -124,12 +123,13 @@ int e2ap_handle_ric_subscription_request(ric_agent_info_t *ric,uint32_t stream,
             ptr++) {
 
         E2AP_RICsubscriptionRequest_IEs_t* rie = (E2AP_RICsubscriptionRequest_IEs_t *)*ptr;
-        if (rie->id == E2AP_ProtocolIE_ID_id_RICrequestID) {
+
+        if  (rie->value.present == E2AP_RICsubscriptionRequest_IEs__value_PR_RICrequestID) {
             rs->request_id = rie->value.choice.RICrequestID.ricRequestorID;
             rs->instance_id = rie->value.choice.RICrequestID.ricInstanceID;
-        } else if (rie->id == E2AP_ProtocolIE_ID_id_RANfunctionID) {
+        } else if (rie->value.present == E2AP_RICsubscriptionRequest_IEs__value_PR_RANfunctionID) {
             rs->function_id = rie->value.choice.RANfunctionID;
-        } else if (rie->id == E2AP_ProtocolIE_ID_id_RICsubscriptionDetails) {
+        } else if (rie->value.present == E2AP_RICsubscriptionRequest_IEs__value_PR_RICsubscriptionDetails) {
             E2AP_RICeventTriggerDefinition_t *rtd = &rie->value.choice.RICsubscriptionDetails.ricEventTriggerDefinition;
 
             if (rtd->size > 0 && rtd->size < E2SM_MAX_DEF_SIZE) {
@@ -143,7 +143,6 @@ int e2ap_handle_ric_subscription_request(ric_agent_info_t *ric,uint32_t stream,
                 // XXX: protocol error?
             }
 
-#ifdef SHAD
             E2AP_RICactions_ToBeSetup_List_t *ral = &rie->value.choice.RICsubscriptionDetails.ricAction_ToBeSetup_List;
 
             for (int i = 0; i < ral->list.count; ++i) {
@@ -168,7 +167,6 @@ int e2ap_handle_ric_subscription_request(ric_agent_info_t *ric,uint32_t stream,
                   LIST_INSERT_BEFORE(LIST_FIRST(&rs->action_list),ra,actions);
                 }
             }
-#endif
         }
     }
 
@@ -189,7 +187,7 @@ int e2ap_handle_ric_subscription_request(ric_agent_info_t *ric,uint32_t stream,
         E2AP_ERROR("failed to generate RICsubscriptionResponse (ranid %u)\n", ric->ranid);
         goto errout;
     }
-   
+
     ric_ran_function_id_t* function_id = (ric_ran_function_id_t *)calloc(1, sizeof(ric_ran_function_id_t));
     *function_id = func->function_id;
     ret = timer_setup(1, 0, TASK_RIC_AGENT, ric->ranid, TIMER_PERIODIC, (void *)function_id, &ric->e2sm_kpm_timer_id);
@@ -228,210 +226,194 @@ int e2ap_handle_ric_subscription_request(ric_agent_info_t *ric,uint32_t stream,
 int e2ap_handle_ric_subscription_delete_request(
   ric_agent_info_t *ric,uint32_t stream,E2AP_E2AP_PDU_t *pdu)
 {
-  E2AP_RICsubscriptionDeleteRequest_t *req;
-  E2AP_RICsubscriptionDeleteRequest_IEs_t *rie,**ptr;
-  long request_id;
-  long instance_id;
-  ric_ran_function_id_t function_id;
-  ric_subscription_t *rs;
-  int ret;
-  uint8_t *buf;
-  uint32_t len;
-  ric_ran_function_t *func;
-  long cause;
-  long cause_detail;
+    E2AP_RICsubscriptionDeleteRequest_t *req;
+    E2AP_RICsubscriptionDeleteRequest_IEs_t *rie,**ptr;
+    long request_id;
+    long instance_id;
+    ric_ran_function_id_t function_id;
+    ric_subscription_t *rs;
+    int ret;
+    uint8_t *buf;
+    uint32_t len;
+    ric_ran_function_t *func;
+    long cause;
+    long cause_detail;
 
-  DevAssert(pdu != NULL);
-  req = &pdu->choice.initiatingMessage.value.choice.RICsubscriptionDeleteRequest;
+    DevAssert(pdu != NULL);
+    req = &pdu->choice.initiatingMessage.value.choice.RICsubscriptionDeleteRequest;
 
-  E2AP_INFO("Received RICsubscriptionDeleteRequest from ranid %u\n",ric->ranid);
+    E2AP_INFO("Received RICsubscriptionDeleteRequest from ranid %u\n",ric->ranid);
 
-  for (ptr = req->protocolIEs.list.array;
-       ptr < &req->protocolIEs.list.array[req->protocolIEs.list.count];
-       ptr++) {
-    rie = (E2AP_RICsubscriptionDeleteRequest_IEs_t *)*ptr;
-    if (rie->id == E2AP_ProtocolIE_ID_id_RICrequestID) {
-      request_id = rie->value.choice.RICrequestID.ricRequestorID;
-      instance_id = rie->value.choice.RICrequestID.ricInstanceID;
+    for (ptr = req->protocolIEs.list.array;
+            ptr < &req->protocolIEs.list.array[req->protocolIEs.list.count];
+            ptr++) {
+        rie = (E2AP_RICsubscriptionDeleteRequest_IEs_t *)*ptr;
+        if (rie->id == E2AP_ProtocolIE_ID_id_RICrequestID) {
+            request_id = rie->value.choice.RICrequestID.ricRequestorID;
+            instance_id = rie->value.choice.RICrequestID.ricInstanceID;
+        }
+        else if (rie->id == E2AP_ProtocolIE_ID_id_RANfunctionID) {
+            function_id = rie->value.choice.RANfunctionID;
+        }
     }
-    else if (rie->id == E2AP_ProtocolIE_ID_id_RANfunctionID) {
-      function_id = rie->value.choice.RANfunctionID;
+
+    func = ric_agent_lookup_ran_function(function_id);
+    if (!func) {
+        E2AP_ERROR("failed to find ran_function %ld\n",function_id);
+        cause = E2AP_Cause_PR_ricRequest;
+        cause_detail = E2AP_CauseRIC_ran_function_id_Invalid;
+        goto errout;
     }
-  }
 
-  func = ric_agent_lookup_ran_function(function_id);
-  if (!func) {
-    E2AP_ERROR("failed to find ran_function %ld\n",function_id);
-    cause = E2AP_Cause_PR_ricRequest;
-    cause_detail = E2AP_CauseRIC_ran_function_id_Invalid;
-    goto errout;
-  }
+    rs = ric_agent_lookup_subscription(ric,request_id,instance_id,function_id);
+    if (!rs) {
+        E2AP_ERROR("failed to find subscription %ld/%ld/%ld\n", request_id,instance_id,function_id);
+        cause = E2AP_Cause_PR_ricRequest;
+        cause_detail = E2AP_CauseRIC_request_id_unknown;
+        goto errout;
+    }
 
-  rs = ric_agent_lookup_subscription(ric,request_id,instance_id,function_id);
-  if (!rs) {
-    E2AP_ERROR("failed to find subscription %ld/%ld/%ld\n",
-	       request_id,instance_id,function_id);
-    cause = E2AP_Cause_PR_ricRequest;
-    cause_detail = E2AP_CauseRIC_request_id_unknown;
-    goto errout;
-  }
+    ret = func->model->handle_subscription_del(ric, rs, 0, &cause, &cause_detail);
+    if (ret) {
+        E2AP_ERROR("failed to remove subscription to ran_function %ld\n", rs->function_id);
+        goto errout;
+    }
 
-  ret = func->model->handle_subscription_del(ric,rs,0,&cause,&cause_detail);
-  if (ret) {
-    E2AP_ERROR("failed to remove subscription to ran_function %ld\n",
-	       rs->function_id);
-    goto errout;
-  }
+    ret = e2ap_generate_ric_subscription_delete_response(ric, request_id, instance_id, function_id, &buf, &len);
+    if (ret) {
+        E2AP_ERROR("failed to generate RICsubscriptionDeleteResponse (ranid %u)\n", ric->ranid);
+        cause = E2AP_Cause_PR_protocol;
+        cause_detail = E2AP_CauseProtocol_unspecified;
+        goto errout;
+    } else {
+        ric_agent_send_sctp_data(ric,stream,buf,len);
+    }
 
-  ret = e2ap_generate_ric_subscription_delete_response(
-    ric,request_id,instance_id,function_id,&buf,&len);
-  if (ret) {
-    E2AP_ERROR("failed to generate RICsubscriptionDeleteResponse (ranid %u)\n",
-	       ric->ranid);
-    cause = E2AP_Cause_PR_protocol;
-    cause_detail = E2AP_CauseProtocol_unspecified;
-    goto errout;
-  }
-  else {
-    ric_agent_send_sctp_data(ric,stream,buf,len);
-  }
+    return 0;
 
-  return 0;
-
- errout:
-  ret = e2ap_generate_ric_subscription_delete_failure(
+errout:
+    ret = e2ap_generate_ric_subscription_delete_failure(
     ric,request_id,instance_id,function_id,cause,cause_detail,&buf,&len);
-  if (ret) {
-    E2AP_ERROR("failed to generate RICsubscriptionDeleteFailure (ranid %u)\n",
-	       ric->ranid);
-  }
-  else {
-    ric_agent_send_sctp_data(ric,stream,buf,len);
-  }
+    if (ret) {
+        E2AP_ERROR("failed to generate RICsubscriptionDeleteFailure (ranid %u)\n", ric->ranid);
+    } else {
+        ric_agent_send_sctp_data(ric,stream,buf,len);
+    }
 
-  return ret;
+    return ret;
 }
 
-int e2ap_handle_ric_service_query(ric_agent_info_t *ric,uint32_t stream,
-				  E2AP_E2AP_PDU_t *pdu)
+int e2ap_handle_ric_service_query(ric_agent_info_t *ric,uint32_t stream, E2AP_E2AP_PDU_t *pdu)
 {
-  uint8_t *buf;
-  uint32_t len;
-  int ret;
+    uint8_t *buf;
+    uint32_t len;
+    int ret;
 
-  E2AP_INFO("Received RICserviceQuery from ranid %u\n",ric->ranid);
+    E2AP_INFO("Received RICserviceQuery from ranid %u\n",ric->ranid);
 
-  /*
-   * NB: we never add, modify, or remove service models or functions, so
-   * this is a noop for us.
-   */
-  ret = e2ap_generate_ric_service_update(ric,&buf,&len);
-  if (ret) {
-    E2AP_ERROR("failed to generate RICserviceUpdate (ranid %u)\n",
-	       ric->ranid);
-    return -1;
-  }
-  else {
-    ric_agent_send_sctp_data(ric,stream,buf,len);
-  }
+    /*
+    * NB: we never add, modify, or remove service models or functions, so
+    * this is a noop for us.
+    */
+    ret = e2ap_generate_ric_service_update(ric,&buf,&len);
+    if (ret) {
+        E2AP_ERROR("failed to generate RICserviceUpdate (ranid %u)\n", ric->ranid);
+        return -1;
+    } else {
+        ric_agent_send_sctp_data(ric,stream,buf,len);
+    }
 
-  return 0;
+    return 0;
 }
 
-int e2ap_handle_reset_request(ric_agent_info_t *ric,uint32_t stream,
-			      E2AP_E2AP_PDU_t *pdu)
+int e2ap_handle_reset_request(ric_agent_info_t *ric,uint32_t stream, E2AP_E2AP_PDU_t *pdu)
 {
-  uint8_t *buf;
-  uint32_t len;
-  int ret;
+    uint8_t *buf;
+    uint32_t len;
+    int ret;
 
-  E2AP_INFO("Received RICresetRequest from ranid %u\n",ric->ranid);
+    E2AP_INFO("Received RICresetRequest from ranid %u\n",ric->ranid);
 
-  ric_agent_reset(ric);
+    ric_agent_reset(ric);
 
-  ret = e2ap_generate_reset_response(ric,&buf,&len);
-  if (ret) {
-    E2AP_ERROR("failed to generate RICresetResponse (ranid %u)\n",
-	       ric->ranid);
-    return -1;
-  }
-  else {
-    ric_agent_send_sctp_data(ric,stream,buf,len);
-  }
+    ret = e2ap_generate_reset_response(ric,&buf,&len);
+    if (ret) {
+        E2AP_ERROR("failed to generate RICresetResponse (ranid %u)\n", ric->ranid);
+        return -1;
+    } else {
+        ric_agent_send_sctp_data(ric,stream,buf,len);
+    }
 
-  return 0;
+    return 0;
 }
 
 int e2ap_handle_message(ric_agent_info_t *ric,int32_t stream,
 			const uint8_t * const buf,const uint32_t buflen)
 {
-  E2AP_E2AP_PDU_t pdu;
-  int ret;
+    E2AP_E2AP_PDU_t pdu;
+    int ret;
 
-  DevAssert(buf != NULL);
+    DevAssert(buf != NULL);
 
-  memset(&pdu,0,sizeof(pdu));
-  ret = e2ap_decode_pdu(&pdu,buf,buflen);
-  if (ret < 0) {
-    E2AP_ERROR("failed to decode PDU\n");
-    return -1;
-  }
+    memset(&pdu,0,sizeof(pdu));
+    ret = e2ap_decode_pdu(&pdu,buf,buflen);
+    if (ret < 0) {
+        E2AP_ERROR("failed to decode PDU\n");
+        return -1;
+    }
 
-  switch (pdu.present) {
-  case E2AP_E2AP_PDU_PR_initiatingMessage:
-    switch (pdu.choice.initiatingMessage.procedureCode) {
-    case E2AP_ProcedureCode_id_RICsubscription:
-      ret = e2ap_handle_ric_subscription_request(ric,stream,&pdu);
-      break;
-    case E2AP_ProcedureCode_id_RICsubscriptionDelete:
-      ret = e2ap_handle_ric_subscription_delete_request(ric,stream,&pdu);
-      break;
-    case E2AP_ProcedureCode_id_RICserviceQuery:
-      ret = e2ap_handle_ric_service_query(ric,stream,&pdu);
-      break;
-    case E2AP_ProcedureCode_id_Reset:
-      ret = e2ap_handle_reset_request(ric,stream,&pdu);
-      break;
-    default:
-      E2AP_WARN("unsupported initiatingMessage procedure %ld (ranid %u)\n",
-		pdu.choice.initiatingMessage.procedureCode,ric->ranid);
-      ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_E2AP_E2AP_PDU,&pdu);
-      return -1;
-    };
-    break;
-  case E2AP_E2AP_PDU_PR_successfulOutcome:
-    switch (pdu.choice.successfulOutcome.procedureCode) {
-    case E2AP_ProcedureCode_id_E2setup:
-      ret = e2ap_handle_e2_setup_response(ric,stream,&pdu);
-      break;
-    default:
-      E2AP_WARN("unsupported successfulOutcome procedure %ld (ranid %u)\n",
-		pdu.choice.initiatingMessage.procedureCode,ric->ranid);
-      ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_E2AP_E2AP_PDU,&pdu);
-      return -1;
-    };
-    break;
-  case E2AP_E2AP_PDU_PR_unsuccessfulOutcome:
-    switch (pdu.choice.unsuccessfulOutcome.procedureCode) {
-    case E2AP_ProcedureCode_id_E2setup:
-      ret = e2ap_handle_e2_setup_failure(ric,stream,&pdu);
-      break;
-    default:
-      E2AP_WARN("unsupported unsuccessfulOutcome procedure %ld (ranid %u)\n",
-		pdu.choice.initiatingMessage.procedureCode,ric->ranid);
-      ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_E2AP_E2AP_PDU,&pdu);
-      return -1;
-    };
-    break;
-  default:
-    E2AP_ERROR("unsupported presence %u (ranid %u)\n",
-	       pdu.present,ric->ranid);
+    switch (pdu.present) {
+        case E2AP_E2AP_PDU_PR_initiatingMessage:
+            switch (pdu.choice.initiatingMessage.procedureCode) {
+                case E2AP_ProcedureCode_id_RICsubscription:
+                    ret = e2ap_handle_ric_subscription_request(ric,stream,&pdu);
+                    break;
+                case E2AP_ProcedureCode_id_RICsubscriptionDelete:
+                    ret = e2ap_handle_ric_subscription_delete_request(ric,stream,&pdu);
+                    break;
+                case E2AP_ProcedureCode_id_RICserviceQuery:
+                    ret = e2ap_handle_ric_service_query(ric,stream,&pdu);
+                    break;
+                case E2AP_ProcedureCode_id_Reset:
+                    ret = e2ap_handle_reset_request(ric,stream,&pdu);
+                    break;
+                default:
+                    E2AP_WARN("unsupported initiatingMessage procedure %ld (ranid %u)\n",
+                            pdu.choice.initiatingMessage.procedureCode,ric->ranid);
+                    ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_E2AP_E2AP_PDU,&pdu);
+                    return -1;
+            };
+            break;
+        case E2AP_E2AP_PDU_PR_successfulOutcome:
+            switch (pdu.choice.successfulOutcome.procedureCode) {
+                case E2AP_ProcedureCode_id_E2setup:
+                    ret = e2ap_handle_e2_setup_response(ric,stream,&pdu);
+                    break;
+                default:
+                    E2AP_WARN("unsupported successfulOutcome procedure %ld (ranid %u)\n", pdu.choice.initiatingMessage.procedureCode,ric->ranid);
+                    ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_E2AP_E2AP_PDU,&pdu);
+                    return -1;
+            };
+            break;
+        case E2AP_E2AP_PDU_PR_unsuccessfulOutcome:
+            switch (pdu.choice.unsuccessfulOutcome.procedureCode) {
+                case E2AP_ProcedureCode_id_E2setup:
+                    ret = e2ap_handle_e2_setup_failure(ric,stream,&pdu);
+                    break;
+                default:
+                    E2AP_WARN("unsupported unsuccessfulOutcome procedure %ld (ranid %u)\n", pdu.choice.initiatingMessage.procedureCode,ric->ranid);
+                    ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_E2AP_E2AP_PDU,&pdu);
+                    return -1;
+            };
+            break;
+        default:
+            E2AP_ERROR("unsupported presence %u (ranid %u)\n", pdu.present,ric->ranid);
+            ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_E2AP_E2AP_PDU,&pdu);
+            return -1;
+    }
+
     ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_E2AP_E2AP_PDU,&pdu);
-    return -1;
-  }
-
-  ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_E2AP_E2AP_PDU,&pdu);
-  return ret;
+    return ret;
 }
 
 int e2ap_handle_timer_expiry(ric_agent_info_t *ric, long timer_id, void* arg) {
